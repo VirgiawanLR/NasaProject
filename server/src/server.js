2b94ec7396c4be4cb8a294dec28b9dc947cae579
VirgiawanLR <116149015+VirgiawanLR@users.noreply.github.com>
2023-09-14 09:29:30 +0700

    FEAT: https
    add a certificate (self-signed) and key for https communication

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
-const http = require("http");
+const fs = require("fs");
+const https = require("https");
 const { loadPlanet } = require("./models/planets.model");
 const { loadLaunchesData } = require("./models/launches.model");
 const db = require("./database/db.config");
@@ -7,7 +8,13 @@ require("dotenv").config();
 const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
-const server = http.createServer(app);
+const server = https.createServer(
+  {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+  },
+  app
+);
 
 async function serverListen() {
   db.connectingDB()
@@ -26,3 +33,17 @@ async function serverListen() {
 }
 
 serverListen();
+
+// make a self-signed certificate for a development => openssl
+// openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
+
+// explaination :
+// openssl req => requesting a new certificate
+// -x509 => self-sign certificate
+// -newkey => secret use in the encryption / private key
+// rsa:4096 => rsa encryption format, a strong format of an encryption, widely used.
+// the 4096 is the size of key (bits), larger size is stronger
+// -nodes => access key without need assign new password
+// -keyout key.pem => the output file that contain the key
+// -out cert.pem => the output file for the certificate, in this case is self-signed.
+// -days 365 =>
