5e42894289528f3dad9e4aada19ec9c25efc4914
VirgiawanLR <116149015+VirgiawanLR@users.noreply.github.com>
2023-09-25 11:18:26 +0700

    Build pipeline workflow

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,24 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors"); // cross origin, allowing/not allowing an IP to access our server
 const path = require("path");
 const morgan = require("morgan");
 const apiV1 = require("./routes/api");
+const helmet = require("helmet"); // for a comprehensive security, read the documentation for complete
+// security handled by helmet
 
 const app = express();
-
+app.use(helmet());
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "..", "public")));
+// for allowing direct-accessing our public folder in our server
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000", // for whitelisting an IP to access our server
   })
 );
-app.use(morgan("combined"));
+app.use(morgan("combined")); // to consoling every API hit to our server
 
 app.use("/api/v1", apiV1);
 app.get("/*", (req, res) => {
